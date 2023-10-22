4903321a94de1c6c19022dba324522ab95a16aff
deordone <deordone@ucsc.edu>
2023-10-22 16:13:17 -0700

    Lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,20 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Rock Paper Scissors
+// Author: David Ordonez <derdone@ucsc.edu
+// Date: 23 October 2023
+
+//Pseudocode (ChatGPT)
+
+// Define the game choices (Rock, Paper, Scissors).
+// Initialize player scores (Player1Score and Player2Score).
+// Create a function to display the game menu with choices.
+// Create a function to get the player's choice (Player1Choice).
+// Randomly generate a choice for Player 2 (Player2Choice).
+// Create a function to determine the winner of the round (DetermineRoundWinner).
+// Update the scores based on the round result (UpdateScores).
+// Display the results of the current round (DisplayRoundResults).
+// Check for the game's end condition (GameOver).
+// Display the final game results.
+// Ask if the players want to play again.
 
 // Constants
 

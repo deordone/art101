274a2258a6c38fd67a1b69bd303f96ca9d85c5c9
deordone <deordone@ucsc.edu>
2023-10-24 20:13:45 -0700

    Lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,18 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Declare Variables
+// Author: David Ordonez
+// Date: 24 October 2023
 
-// Constants
+// Variables
+var make = "Nissan";
+var model = "GT-R";
+var color = "Black";
+var year = 2012;
 
-// Functions
+// Calculate Age
+var age = 2023 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+document.writeln("Make: "+ make + "<br>");
+document.writeln("Model: "+ model + "<br>");
+document.writeln("Color: "+ color + "<br>");
+document.writeln("Year: "+ year + "<br>");
+document.writeln("Age: "+ age + "<br>");

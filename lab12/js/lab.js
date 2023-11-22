b5e709420c2cfb8aa45ea12bf172867810757941
deordone <deordone@ucsc.edu>
2023-11-21 19:35:04 -0800

    Lab 12

---
+++ lab12/js/lab.js
@@ -0,0 +1,28 @@
+// index.js - Conditional
+// Author: David Ordonez
+// Date: 24 November 2023
+
+function sortingHat(str) {
+  len = str.length;
+  mod = len % 4;
+  if (mod == 0) {
+    return "Gryffindor"
+  }
+  else if (mod == 1) {
+    return "Ravenclaw"
+  }
+  else if (mod == 2) {
+    return "Slytherin"
+  }
+  else if (mod ==  3) {
+    return "Hufflepuff"
+  }
+}
+
+var myButton =  document.getElementById("button");
+myButton.addEventListener("click", function() {
+  var name = document.getElementById("input").value;
+  var house = sortingHat(name);
+  newText = "<p>The Sorting Hat has sorted you into " + house + "</p>";
+  document.getElementById("output").innerHTML = newText;
+})

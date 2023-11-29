0525898a90b13864af2da08b67639437fd43d4e3
deordone <deordone@ucsc.edu>
2023-11-29 13:16:00 -0800

    lab 12 debug

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -2,27 +2,53 @@
 // Author: David Ordonez
 // Date: 24 November 2023
 
-function sortingHat(str) {
-  len = str.length;
-  mod = len % 4;
-  if (mod == 0) {
-    return "Gryffindor"
-  }
-  else if (mod == 1) {
-    return "Ravenclaw"
-  }
-  else if (mod == 2) {
-    return "Slytherin"
-  }
-  else if (mod ==  3) {
-    return "Hufflepuff"
-  }
+/* Updated sortingHat function with array of houses and descriptions */
+function sortingHat(name) {
+  // Calculate a checksum or hash based on the name's characters
+  const checksum = calculateChecksum(name);
+
+  // Define an array of Hogwarts houses
+  const houses = ["Gryffindor", "Ravenclaw", "Slytherin", "Hufflepuff"];
+
+  // Define an array of corresponding house descriptions
+  const houseDescriptions = [
+    "Courage, bravery, nerve, and chivalry",
+    "Intelligence, creativity, learning, and wit",
+    "Ambition, cunning, leadership, and resourcefulness",
+    "Hard work, dedication, patience, loyalty, and fair play"
+  ];
+
+  // Use the checksum to determine the index of the house
+  const houseIndex = checksum % houses.length;
+
+  // Return an object containing the house name and description
+  return {
+    name: houses[houseIndex],
+    description: houseDescriptions[houseIndex]
+  };
 }
 
-var myButton =  document.getElementById("button");
-myButton.addEventListener("click", function() {
+/* Function to calculate checksum based on ASCII values of characters */
+function calculateChecksum(str) {
+  let checksum = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    checksum += str.charCodeAt(i);
+  }
+
+  return checksum;
+}
+
+/* Event listener for the button click */
+var myButton = document.getElementById("button");
+myButton.addEventListener("click", function () {
+  // Get the input value (user's name)
   var name = document.getElementById("input").value;
-  var house = sortingHat(name);
-  newText = "<p>The Sorting Hat has sorted you into " + house + "</p>";
+
+  // Call the sortingHat function to get the Hogwarts house and description
+  var result = sortingHat(name);
+
+  // Display the result
+  var newText = `<p>The Sorting Hat has sorted you into ${result.name}. ${result.description}</p>`;
   document.getElementById("output").innerHTML = newText;
-})
+});

b8f687b95c8469fe8d1fee66283eaca89cd8a698
deordone <deordone@ucsc.edu>
2023-11-29 12:31:07 -0800

    lab 11 debug

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -2,20 +2,37 @@
 // Author: David Ordonez
 // Date: 20 November 2023
 
-
 // Sorts the characters of a string in alphabetical order.
 function sortString(inputString) {
-    // We have to convert our string to an array and back again to sort it
-    return inputString.split('').sort().join('');
-    
-  }
+  return inputString.split('').sort().join('');
+}
+
+// Generates a random anagram for a given string.
+function generateAnagram(inputString) {
+  const shuffled = inputString.split('').sort(() => 0.5 - Math.random()).join('');
+  return shuffled !== inputString ? shuffled : generateAnagram(inputString);
+}
 
 // click listener for button
-$("#submit").click(function(){
-    // get value of input field
-    const userName = $("#user-name").val();
-    // now let's sort it
-    userNameSorted = sortString(userName);
-    // append a new div to our output div
-    $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+$("#submit").click(function () {
+  // Create an array to store input values
+  const inputValues = [];
+
+  // Loop through each input field with class "user-input"
+  $(".user-input").each(function () {
+      const inputValue = $(this).val();
+      const sortedValue = sortString(inputValue);
+      const anagram = generateAnagram(inputValue);
+
+      // Push the original, sorted, and anagram values to the array
+      inputValues.push({ original: inputValue, sorted: sortedValue, anagram: anagram });
+  });
+
+  // Create new <p> elements for each result and append to the output div
+  $("#output").empty(); // Clear existing content
+
+  inputValues.forEach((value, index) => {
+      const resultText = `Input ${index + 1}: Original - ${value.original}, Sorted - ${value.sorted}, Anagram - ${value.anagram}`;
+      $("#output").append(`<p>${resultText}</p>`);
+  });
 });
